c6f3d6ec4e37a06f4414d023fe66b31130247032
Sohyeon Kim <jhkimsh1104@likelion.org>
2018-05-06 03:20:24 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index #메인페이지 루트페이지
+  end
+  
+  def clickIdea #상품 개별창
+  end
+  
+  def updateIdea #상품 개별창에서 내용 수정
+  end
+  
+  def ideaList # 전체 상품 리스트
+  end
+  
+  def uploadIdea #상품 업로드
+  end
+
+end
